96044bdb8396fa3ccd3a60d7bf75b86f6b9d7d9d
danissiimmo <64765625+danissiimmo@users.noreply.github.com>
2024-02-10 14:20:13 +0300

    Add files via upload

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("popup-trigger").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "block";
-  });
-  
-  document.getElementById("close").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "none";
-  });
