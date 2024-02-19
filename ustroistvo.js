97f5d5dde00ee70bf15b4ffc2ee73c420a94598d
danissiimmo <64765625+danissiimmo@users.noreply.github.com>
2024-02-19 19:13:37 +0300

    Add files via upload

---
+++ ustroistvo.js
@@ -0,0 +1,9 @@
+(function() {
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+        window.location.href = "mobile.html";
+    } else {
+        window.location.href = "index.html";
+    }
+  })();
+  
